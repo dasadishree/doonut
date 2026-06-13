078801cfc83c8da18454304b9a62b9c34e6b566f
dasadishree <dasadishree@gmail.com>
2026-06-13 14:31:00 -0400

    mug

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,48 +1,38 @@
-import { useState, useRef } from 'react'
+import {  useRef, Suspense} from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, useGLTF } from '@react-three/drei'
 import './App.css'
 
-// temporary 3d cube
-function SpinningCube() {
-  const meshRef = useRef()
-  useFrame(() => {
-    if(meshRef.current) {
-      meshRef.current.rotation.x +=0.01
-      meshRef.current.rotation.y += 0.01
+// mug
+function Mug() {
+  const { scene } = useGLTF('/mug.glb')
+  const mugRef = useRef()
+  useFrame(()=>{
+    if(mugRef.current){
+      mugRef.current.rotation.y += 0.008
     }
   })
-  return(
-    <mesh>
-      <boxGeometry args={[1,1,1]}/>
-      <meshStandardMaterial color="#fff7b00"/>
-    </mesh>
-  )
+  return <primitive ref={mugRef} object={scene} scale={5} position={[0, -1, 0]}/>
 }
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="app-container">
-      <header className="navbar">
-        <div className="logo">Donut 3D</div>
-        <nav>
-          <button onClick={()=>setCount(count+1)}>Likes: {count}</button>
-        </nav>
-      </header>
-
-      <main className="main=content">
+      <main className="main-content">
         <section className="hero">
           <h1>Adishree's 3D Progress</h1>
           <p>See my progress in using Blender / CAD!</p>
         </section>
 
         <section className="canvas-container">
-          <Canvas camera={{position: [0,0,5]}}>
-            <ambientLight intensity={0.5}/>
-            <directionalLight position={[10, 10, 5]} intensity={1}/>
-            <SpinningCube/>
+          <Canvas camera={{position: [0,2,5], fov:45}}>
+            <ambientLight intensity={0.7}/>
+            <directionalLight position={[10, 10, 5]} intensity={1.5}/>
+            <directionalLight position={[-10, 5, -5]} intensity={0.5}/>
+
+            <Suspense fallback={null}>
+              <Mug/>
+            </Suspense>
             <OrbitControls enableZoom={false}/>
           </Canvas>
         </section>
